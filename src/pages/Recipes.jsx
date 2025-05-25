4653f8fe69f428fde76ad9a1e601ddc92ba8748a
Surajverma <sv63619@gmail.com>
2025-05-25 20:25:54 +0530

    delete func

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,29 +1,52 @@
-import React, { useContext } from 'react'
-import { recipecontext } from '../context/Recipecontext'
+import React, { useContext } from "react";
+import { recipecontext } from "../context/Recipecontext";
 
 const Recipes = () => {
+  const [data, setData] = useContext(recipecontext);
+  
+  function deleteHandler(id) {
+    let recipe = data.filter((item) => item.id !== id);
+    setData(recipe)
+  }
 
-    const [data] = useContext(recipecontext)
   return (
-    <div className='w-full flex items-center gap-5 mt-[12rem] px-[4rem]'>
-      {
-        data.map((item)=>(
-            <div className='w-[40%] h-[25rem] bg-[#80a572] px-3 py-3 rounded-lg flex flex-col gap-3' key={item.id}>
-                <div className='w-full h-[12rem] overflow-hidden rounded-lg flex-1'>
-                    <img className='w-full h-full object-cover ' src={item.image} alt="" />
-                </div>
-                <h1><strong>name</strong> : {item.title}</h1>
-                <h1 className='break-words whitespace-normal w-full'><strong>desc</strong> : {item.description}</h1>
-                <h1 className='break-words whitespace-normal w-full'><strong>ingredients</strong> : {item.ingredients}</h1>
-                
-                  <button className='bg-emerald-600 px-3 py-2 rounded-md'>Add to favourites</button>
-                
-                
-            </div>
-        ))
-      }
+    <div className="w-full flex items-center gap-5 mt-[12rem] px-[4rem]">
+      {data.map((item) => (
+        <div
+          className="w-[40%] h-[25rem] bg-[#80a572] px-3 py-3 rounded-lg flex flex-col gap-3"
+          key={item.id}
+        >
+          <div className="w-full h-[12rem] overflow-hidden rounded-lg flex-1">
+            <img
+              className="w-full h-full object-cover "
+              src={item.image}
+              alt=""
+            />
+          </div>
+          <h1>
+            <strong>name</strong> : {item.title}
+          </h1>
+          <h1 className="break-words whitespace-normal w-full">
+            <strong>desc</strong> : {item.description}
+          </h1>
+          <h1 className="break-words whitespace-normal w-full">
+            <strong>ingredients</strong> : {item.ingredients}
+          </h1>
+          <div className="flex gap-[2rem]">
+            <button className="bg-emerald-600 px-3 py-2 rounded-md">
+              Add to favourites
+            </button>
+            <button
+              onClick={()=>deleteHandler(item.id)}
+              className="bg-emerald-600 px-3 py-2 rounded-md"
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Recipes
+export default Recipes;
